e458f6b118e963f4440e86d110ae1e2c26df6c89
virlei <rvirlei@gmal.com>
2021-09-30 12:02:48 -0300

    Banco de dados

---
+++ server/src/db/migrations/20210921143133-create-professor.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Professor', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      materia: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      id_escola :{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "escolas",
+          key: "id"
+    },
+    onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+  },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Professor');
+  }
+};
---
+++ server/src/db/models/professor.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const bcrypt = require("bcrypt");
+module.exports = (sequelize, DataTypes) => {
+  class Professor extends Model {
+    static associate(models) {
+      this.belongsTo(models.Escola, { foreignKey: "id_escola" });
+    }
+  };
+  Professor.init({
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: { msg: "E-mail inválido" }
+      }
+    },
+    materia: {
+     type: DataTypes.STRING, 
+     allowNull: false
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(senha) {
+        this.setDataValue("senha", bcrypt.hashSync(senha, 10));
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Professor',
+  });
+  return Professor;
+};
