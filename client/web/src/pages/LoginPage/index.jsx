cdcdb31f9d43453a18a458927e078a6ce8029dd2
Luiz Augusto JR <junior.luiz43@gmail.com>
2021-10-04 11:54:58 -0300

    css

---
+++ client/web/src/pages/LoginPage/index.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React from "react";
+import logo from "../../assets/logo.png";
+
+export function LoginPage() {
+    return (
+        <body className="diagonal-box">
+            <html>
+                <div>
+                    <img src={logo} alt="Logo do site" />
+                </div>
+            </html>
+        </body>
+    );
+}
+
