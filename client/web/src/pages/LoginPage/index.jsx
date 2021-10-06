7dbdb55e0887dd23342221ce420abda34b248105
Luiz Casa <junior.luiz43#gmail.com>
2021-10-05 23:31:00 -0300

    Tela de Login WEB

--- client/web/src/pages/LoginPage/index.jsx
+++ client/web/src/pages/LoginPage/index.jsx
@@ -1,17 +1,45 @@
-import "./styles.css";
 import React from "react";
+import "./styles.css";
 import logo from "../../assets/logo.png";
+import { MdEmail, MdLock } from "react-icons/md";
+
+
+
 
 export function LoginPage() {
     return (
-        <html>
-        <body className="diagonal-box">
-                <div>
-                    <img src={logo} alt="Logo do site" />
+        <div className="login">
+
+            <div className="login-logo">
+                <img src={logo} alt="logo" />
+            </div>
+
+            <div className="login-right">
+                <h1>Entrar no School Follow-UP</h1>
+
+                <div className="loginInputEmail">
+                    <MdEmail />
+                    <input type={Text}
+                        placeholder="digite seu email"
+                    />
                 </div>
-            
-        </body>
-        </html>
+
+                <div className="loginInputPassword">
+                    <MdLock />
+                    <input type={Text}
+                        placeholder="digite sua senha"
+                    />
+                </div>
+
+                <button type="submit">
+                    Login
+                </button>
+
+            </div>
+
+        </div>
     );
 }
+
+
 
