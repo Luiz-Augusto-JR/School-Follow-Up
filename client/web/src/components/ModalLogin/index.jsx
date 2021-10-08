c3bc75f09f264fdbce6dd78a62d1aa90beea6359
Luiz Augusto JR <junior.luiz43@gmail.com>
2021-10-08 11:59:49 -0300

    modal

---
+++ client/web/src/components/ModalLogin/index.jsx
@@ -0,0 +1,29 @@
+import { Modal } from "../Modal";
+import "./styles.css";
+
+export function ModalLogin({ closeModal }) {
+    return (
+        <Modal closeModal={closeModal}>
+            <form className="form-login">
+                <label>
+                    Nome                    
+                </label>
+                <input className="inputModal" type="text" />
+                <label>
+                    E-mail
+                    
+                </label>
+                <input className="inputModal" type="text" />
+                <label>
+                    Senha
+                </label>
+                <input className="inputModal" type="text" />
+                <label>
+                    Confirmar senha                
+                </label>
+                <input className="inputModal" type="text" />
+                <button className="buttonCriar"  >Enviar</button>
+            </form>
+        </Modal>
+    );
+}
--- client/web/src/pages/AdminHome/index.jsx
+++ client/web/src/pages/AdminHome/index.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./stylesAdminHome.css";
 import gif from "../../assets/logoCortadaBranca.gif";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { ModalLogin } from "../../components/ModalLogin";
 
 
-export function AdminHome () {
+export function AdminHome() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     return (
         <div>
-        <div className="bordaSuperior">
-            <h1>Bem -Vindo!</h1>
-        </div>
-        <div className="buttonDiv">
-            <button class="buttonCriar" type="button"><AiFillPlusCircle /><text> CRIAR LOGIN ALUNOS</text></button>
-            <button class="buttonCriar" type="button"><AiFillPlusCircle /><text> CRIAR LOGIN PROFESSORES</text></button>
-        </div>
-        <div className="divMaterias">
-        <button class="buttonMaterias" type="button"><text>MATÉRIAS</text></button>
-        </div>
+            {
+                isModalVisible &&
+                <ModalLogin closeModal={() => setIsModalVisible(false)}>
+                    <button>Enviar</button>
+                </ModalLogin>
+            }            
+            <div className="bordaSuperior">
+                <h1>Bem -Vindo!</h1>
+            </div>
+            <div className="buttonDiv">
+                <button class="buttonCriar" onClick={() => setIsModalVisible(true)}>
+                    <AiFillPlusCircle/><text> CRIAR LOGIN ALUNOS</text>
+                </button>
+                <button class="buttonCriar" type="button">
+                    <AiFillPlusCircle/>
+                    <text> CRIAR LOGIN PROFESSORES</text>
+                </button>
+            </div>
+            <div className="divMaterias">
+                <button class="buttonMaterias" type="button"><text>MATÉRIAS</text></button>
+            </div>
             <div className="home-logo">
                 <img src={gif} alt="logo" />
             </div>
         </div>
- 
+
     );
 }
