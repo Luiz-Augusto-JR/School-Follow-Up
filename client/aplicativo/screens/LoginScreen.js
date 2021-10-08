67516fe5d5a8ff3f511c1c8b08b0675214a374b6
Darlana <darlana.ribeiro.lima@gmail.com>
2021-10-08 10:14:08 -0300

    Tela de Login

---
+++ client/aplicativo/screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+
+export function LoginScreen() {
+  return (
+    <View style={styles.container}>
+        <Image
+         style={styles.stretch}
+         source={require('../components/logo.png')}
+      />
+      <Text></Text>
+      <View style={styles.email}>
+       
+      <Button
+        title="EMAIL"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Text> <br></br> </Text>
+        <View style={styles.senha}>
+          
+         <Button
+        title="SENHA"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+         <Text> <br></br> </Text> 
+       <View style={styles.login}>
+        
+         <Button
+        title="LOGIN"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      
+      />
+       </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+
+  },stretch: {
+    width: 250,
+    height: 250,
+    resizeMode: 'stretch',
+  },
+  email:{
+    width: "50%"
+  },
+  senha:{
+
+  },
+  login:{
+
+  }
+});
+
