f2e46d0978e01c5704d04c2d47c2cc91d607d009
GUILHERME AUGUSTO CAMPOS <guilherme.campos1@alunos.sc.senac.br>
2021-10-11 11:51:46 -0300

    tela perfil

--- client/aplicativo/App.js
+++ client/aplicativo/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { HomeScreen } from './screens/HomeScreen';
+import { PerfilScreen } from './screens/PerfilScreen';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <PerfilScreen />
   );
 }
 
--- client/aplicativo/screens/PerfilScreen.js
+++ client/aplicativo/screens/PerfilScreen.js
@@ -1,46 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 export function PerfilScreen() {
     return (
 
         <View style={styles.container}>
-        <Image
-          style={styles.tinyLogo}
-          source={require('icons8-user-24(1).png')}
-        />
-        <Image
-          style={styles.tinyLogo}
-          source={{uri: 'https://icons8.com/icons/set/user'}}
-        />
-        <Image
-
-
-
-        <View style={styles.container}>                        
             <View style={styles.perfil}>
-
-
-                <Text>PERFIL</Text>
-                <view style={styles.perfil}>
-                   
-                    <Text> <br>BEM VINDO %ALUNO%</br> </Text>
-                    <View style={styles.perfil}>
-
-                       
-                    </View>
-                </View>
+            <Text>PERFIL</Text>
+                <EvilIcons name="user" size={200} color="black" />
+                <Text>BEM VINDO %ALUNO%</Text>
             </View>
+
+            <View style={styles.nav}>
+                
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 2,
+        flex: 1,
         backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
 
+    },
+    perfil: {
+        alignItems: 'center'
+    },
+    nav: {
 
+    }
 });
+
