2b018b4d24d249139c0f2bb5b32ed1479b4101b8
Luiz Augusto JR <junior.luiz43@gmail.com>
2021-10-15 11:58:10 -0300

    att

--- client/web/src/pages/Materias/index.jsx
+++ client/web/src/pages/Materias/index.jsx
@@ -6,23 +6,23 @@ import { Modal } from "../../components/Modal";
 
 export function Materias() {
     const [materias, setMaterias] = useState([
-        { 
-            id: 1, 
-            nome: "Matematica", 
+        {
+            id: 1,
+            nome: materias.nome,
             alunos: [
                 { id: 1, nome: "Aluno 01" },
                 { id: 2, nome: "Aluno 02" },
-            ], 
-            professor: { id: 1, nome: "Professor"}
+            ],
+            professor: { id: 1, nome: "Professor" }
         },
-        { 
-            id: 2, 
-            nome: "Geografia", 
+        {
+            id: 2,
+            nome: "Geografia",
             alunos: [
                 { id: 1, nome: "Aluno 01" },
                 { id: 2, nome: "Aluno 02" },
-            ], 
-            professor: { id: 1, nome: "Professor"}
+            ],
+            professor: { id: 1, nome: "Professor" }
         }
     ]);
 
@@ -31,17 +31,27 @@ export function Materias() {
     const [notas, setNotas] = useState([]);
     const [frequencia, setFrequencia] = useState(null);
 
-    // useEffect(() => {
-    //     (async () => {
-    //         try {
-    //             const materias = (await api.get("/materias")).data;
+    useEffect(() => {
+        (async () => {
+            try {
+                const materias = (await api.get(`/materias`)).data;
+                setMaterias(materias)
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
 
-    //             setMaterias(materias)
-    //         } catch (error) {  
-    //             console.log(error);
-    //         }
-    //     })();
-    // }, []);
+    useEffect(() => {
+        (async () => {
+            try {
+                const professores = (await api.get("/professo")).data;
+                setMaterias(materias)
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
 
     function getInfoMateria(id) {
         const infoMateria = materias.find(materia => materia.id === id);
@@ -52,8 +62,8 @@ export function Materias() {
         async function getNotasFrequenciaAluno() {
             try {
                 const notas = (await api.get(`/alunos/${alunoSelecionado.id}/notas`)).data;
-                const frequencia = (await api.get(`/alunos/${alunoSelecionado.id}/frequencia`)).data;    
-                
+                const frequencia = (await api.get(`/alunos/${alunoSelecionado.id}/frequencia`)).data;
+
                 setNotas(notas);
                 setFrequencia(frequencia);
             } catch (error) {
@@ -69,21 +79,21 @@ export function Materias() {
                 <h1>BEM-VINDO AO SCHOOL FOLLOW-UP!</h1>
             </div>
             <div className="materiasfont">
-            <h2>materias</h2>
+                <h2>materias</h2>
             </div>
             <div className="AlunosEProfessoresfont">
-            <h2>Alunos E Professores</h2>
+                <h2>Alunos E Professores</h2>
             </div>
             <div className="todasMaterias">
-            {
-                materias.map(materia => <h3 key={materia.id} onClick={() => getInfoMateria(materia.id)}>{materia.nome}</h3>)
-            }
+                {
+                    materias.map(materia => <h3 key={materia.id} onClick={() => getInfoMateria(materia.id)}>{materia.nome}</h3>)
+                }
             </div>
             {
                 infoMateria &&
                 <div className="professoresAlunos">
                     <h2>{infoMateria.nome}</h2>
-                    { infoMateria.alunos.map(aluno => <p key={aluno.id} onClick={() => setAlunoSelecionado(aluno)}>{aluno.nome}</p>)}
+                    {infoMateria.alunos.map(aluno => <p key={aluno.id} onClick={() => setAlunoSelecionado(aluno)}>{aluno.nome}</p>)}
                     <h2>{infoMateria.professor.nome}</h2>
                 </div>
             }
@@ -101,10 +111,10 @@ export function Materias() {
                             <th>Frequência</th>
                         </thead>
                     </table>
-                </Modal>                
+                </Modal>
             }
 
-            
+
         </div>
 
     );
